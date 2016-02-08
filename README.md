efb6a4b13fc1c6f597fb53ca92994daa1ea8fc2a
Dane Elwell <dane.elwell@gmail.com>
2016-02-08 12:47:28 +0000

    Update readme, example importer and tasker setup

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 SMS Logger
 ==========
 
-In Tasker on Android you can configure it to make a HTTP POST request on an incoming SMS message. This thing accepts those requests and logs the SMS to a database for later retrieval.
+In Tasker on Android you can configure it to make a HTTP POST request on an incoming SMS message (see screenshots in tasker\_setup for examples of how I did it). This thing accepts those requests and logs the SMS to a database for later retrieval.
 
 The code here is likely terrible. This has been thrown together pretty quickly. The web interface has also been designed so I don't have to write a single line of JS, because why would I do that to myself?
+
+Expects PostgreSQL as a database backend but shouldn't be too hard to make it work with another database.
---
+++ importer/import.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# If you've already got an export of your text messages in CSV format (as
+# created by the SMStoText app), you can use this import script to shove them
+# all into the database in one go. Again, this was also thrown together - do not
+# expect this to be fast or awesome.
+# Change path in the open() statement below to your CSV file and the URL to your
+# installation.
+
+URL = "https://whoever:whatever@wherever.co.uk"
+
+import csv
+import requests
+import time
+import hashlib
+
+with open('import.csv', 'r') as fh:
+    smsreader = csv.reader(fh)
+    for row in smsreader:
+        if row[0] == 'Date':
+            continue
+
+        date = time.strftime('%d-%M-%Y', time.strptime(row[0], "%Y-%M-%d"))
+        timestamp = time.strftime('%H.%M', time.strptime(row[1], "%H:%M:%S"))
+        hashts = time.strftime('%H:%M:00', time.strptime(row[1], "%H:%M:%S"))
+        f = row[4]
+        message = row[5]
+
+        h = hashlib.sha256()
+        h.update("{} {}".format(row[0], hashts))
+        h.update(f)
+        h.update(message)
+
+        resp = requests.get(URL+"/sms?hash={}".format(h.hexdigest()))
+        if resp.status_code == 404:
+            payload = {
+                "time": timestamp,
+                "date": date,
+                "from": f,
+                "message": message,
+            }
+            resp = requests.post(URL+"/sms", payload)
+            print "{}: {}".format(resp.status_code, message)
+        else:
+            print "Duplicate message, not sent: {}".format(message)
